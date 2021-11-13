b8c0cf69e257dd1957dbffffdfccd1649ca240b9
zhuang002 <zhuang001@gmail.com>
2021-11-12 21:33:59 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, id):
+        self.id = id
+        self.name = None
+        self.age = 0
+        self.school = None
+        self.grade = 1
+        self.gender = 'F'
+
+
+student1 = Student(1)
+student2 = Student(2)
+
+print(student1)
+print(student2)
+
